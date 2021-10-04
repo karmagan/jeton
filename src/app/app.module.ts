bbce62eb0345a2dbf9e438922aa5013b1f0b6444
Kadir Armagan <kadir@localhost.localdomain>
2021-10-04 21:37:53 +0100

    Components Initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { BusinessComponent } from './business/business.component';
+import { MethodsComponent } from './methods/methods.component';
+import { AppsComponent } from './apps/apps.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    BusinessComponent,
+    MethodsComponent,
+    AppsComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
